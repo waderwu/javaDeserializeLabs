e9e061fd0d47939dbdf155c3ed499f123d266af3
waderwu <waderwu@163.com>
2021-03-20 13:02:25 +0800

    objectToHexString

--- src/main/java/com/yxxx/javasec/deserialize/Utils.java
+++ src/main/java/com/yxxx/javasec/deserialize/Utils.java
@@ -1,5 +1,9 @@
 package com.yxxx.javasec.deserialize;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Utils {
     public static String bytesTohexString(byte[] bytes){
         if (bytes == null) return null;
@@ -12,5 +16,17 @@ public class Utils {
             ret.append("0123456789abcdef".charAt(b));
         }
         return ret.toString();
+    }
+
+    public static String objectToHexString(Object obj)throws Exception{
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = null;
+        out = new ObjectOutputStream(bos);
+        out.writeObject(obj);
+        out.flush();
+        byte[] bytes = bos.toByteArray();
+        bos.close();
+        String hex = bytesTohexString(bytes);
+        return hex;
     }
 }
