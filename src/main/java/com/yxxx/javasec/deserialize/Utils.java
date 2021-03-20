4d122352ceb0c97483368d73bbf81f8f8313f48a
waderwu <waderwu@163.com>
2021-03-20 12:56:19 +0800

    add Utils.java

---
+++ src/main/java/com/yxxx/javasec/deserialize/Utils.java
@@ -0,0 +1,16 @@
+package com.yxxx.javasec.deserialize;
+
+public class Utils {
+    public static String bytesTohexString(byte[] bytes){
+        if (bytes == null) return null;
+        StringBuilder ret = new StringBuilder(2*bytes.length);
+        for (int i = 0 ; i < bytes.length ; i++) {
+            int b;
+            b = 0x0f & (bytes[i] >> 4);
+            ret.append("0123456789abcdef".charAt(b));
+            b = 0x0f & bytes[i];
+            ret.append("0123456789abcdef".charAt(b));
+        }
+        return ret.toString();
+    }
+}
